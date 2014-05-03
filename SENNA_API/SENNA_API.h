4116438ca32c34c71d744dbd2a563134f22a8211
Deyan Ginev <d.ginev@jacobs-university.de>
2014-05-03 02:14:42 +0200

    turning on warnings and elminating some of the easy ones

--- SENNA_API/SENNA_API.h
+++ SENNA_API/SENNA_API.h
@@ -1,1 +1,35 @@
+// Senna POS tagger
+#include "SENNA_utils.h"
+#include "SENNA_Hash.h"
+#include "SENNA_Tokenizer.h"
+#include "SENNA_POS.h"
+
+typedef struct {
+  /* SENNA inputs */
+  SENNA_Hash *word_hash;
+  SENNA_Hash *caps_hash;
+  SENNA_Hash *suff_hash;
+  SENNA_Hash *gazt_hash;
+  SENNA_Hash *gazl_hash;
+  SENNA_Hash *gazm_hash;
+  SENNA_Hash *gazo_hash;
+  SENNA_Hash *gazp_hash;
+  /* SENNA labels */
+  SENNA_Hash *pos_hash;
+  SENNA_Hash *chk_hash;
+  SENNA_Hash *pt0_hash;
+  SENNA_Hash *ner_hash;
+  SENNA_Hash *vbs_hash;
+  SENNA_Hash *srl_hash;
+  SENNA_Hash *psg_left_hash;
+  SENNA_Hash *psg_right_hash;
+  /* Action objects */
+  SENNA_Tokenizer *tokenizer;
+  SENNA_POS *pos;
+} SENNA_fields;
+
 void* SENNA_new();
+void DESTROY(SENNA_fields* SENNA_object);
+void* SENNA_Tokenize_sentence(SENNA_fields* SENNA_object, char* sentence);
+void* SENNA_POS_sentence(SENNA_fields* SENNA_object, char* sentence);
+void* SENNA_fullproc_sentence(SENNA_fields* SENNA_object, char* sentence);
